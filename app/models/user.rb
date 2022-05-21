5b201a9d510c044e17b22be689d432847209ea84
moomitt <100453545+moomitt@users.noreply.github.com>
2022-05-21 05:42:22 +0000

    [Start] 5-18

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :post_images, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
   
   has_one_attached :profile_images
   
