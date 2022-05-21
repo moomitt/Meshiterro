a97da354705974fd2dcf7b1878edf6dbff8ef518
moomitt <100453545+moomitt@users.noreply.github.com>
2022-05-21 01:36:30 +0000

    [Finish] 5-13

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -19,6 +19,12 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.find(params[:id])
   end
   
+  def destroy
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to post_images_path
+  end
+  
   private
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
   
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
   
   get '/homes/about' => 'homes#about', as: 'about'
 end
