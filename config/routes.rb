f9040f72f3ffc54bf8663dbfb64555f39e8f5c38
moomitt <100453545+moomitt@users.noreply.github.com>
2022-05-21 04:52:26 +0000

    [Finish] 5-16

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
     get '/homes/about' => 'homes#about', as: 'about'
   
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
 
 end
